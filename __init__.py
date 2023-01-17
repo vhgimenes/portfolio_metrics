aaa635eb309a7a019355980a2478b2ab891d76ad
Victor Gimenes <108276273+vhgimenes@users.noreply.github.com>
2023-01-16 21:47:21 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,6 @@
-"""
-Adicionar as extenções 
-https://github.com/ranaroussi/quantstats/blob/main/quantstats/__init__.py
-"""
-# Importando libs externas
-import importlib
-# Importando as funções do pacote
+__all__ = [utils, stats, graphs, metrics]
+
 from . import utils
 from . import stats
 from . import graphs
 from . import metrics
-# Para permitir que mudanças passem a ser usadas diretamente
-importlib.reload(utils)
-importlib.reload(stats)
-importlib.reload(graphs)
-importlib.reload(metrics)
